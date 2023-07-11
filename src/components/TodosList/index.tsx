bee1f8d273c2ad5caa2b9bfbc632f1b8254459d2
Artur Voloshin <nozeilwrk@gmail.com>
2023-07-11 19:00:20 +0300

    fix: replace Todo Wrapper key with uuid

--- src/components/TodosList/index.tsx
+++ src/components/TodosList/index.tsx
@@ -3,17 +3,22 @@ import { TodoItem } from '../TodoItem';
 import { TodoWrapper } from '../TodoWrapper';
 import { useContext } from 'react';
 import { TodosContext } from '@/context';
+import { v4 as uuidv4 } from 'uuid';
 
 export const Todos = () => {
   const todos = useContext(TodosContext);
 
   return (
     <List>
-      {todos.map((item, index) => (
-        <TodoWrapper key={index}>
-          <TodoItem initialValue={item.initialValue} initialIsChecked={item.initialIsChecked} />
-        </TodoWrapper>
-      ))}
+      {todos.map((item) => {
+        const id = uuidv4();
+
+        return (
+          <TodoWrapper key={id}>
+            <TodoItem initialValue={item.initialValue} initialIsChecked={item.initialIsChecked} />
+          </TodoWrapper>
+        );
+      })}
     </List>
   );
 };
